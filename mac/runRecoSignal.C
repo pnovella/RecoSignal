32c1df7eb8eaf6c74a17f7cc95ff5577ae8dc315
pnovella <pau.novella@ific.uv.es>
2015-05-18 18:43:15 +0200

    new macro added: runs RP and RS

--- mac/runRecoSignal.C
+++ mac/runRecoSignal.C
@@ -16,10 +16,10 @@
   rsig->SetMinS2Hits(14);
 
   gate::Centella::instance(gate::NORMAL);
-  gate::Centella::instance()->addInputFile("../../../DATA/DST_3350_000_RP.root");
-  gate::Centella::instance()->addOutputFile("output_dst.root");
-  gate::Centella::instance()->setNevents(10000);
-  gate::Centella::instance()->saveEvents(false);
+  gate::Centella::instance()->addInputFile("/home/pnovella/Physics/NEXT/DATA/DST_3350_000_RP.root");
+  gate::Centella::instance()->addOutputFile("test.root");
+  gate::Centella::instance()->setNevents(100);
+  gate::Centella::instance()->saveEvents(true);
   gate::Centella::instance()->saveHistos(true);
   gate::Centella::instance()->addAlgo("my_algo1",rsig);
     
